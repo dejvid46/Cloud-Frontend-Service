6e69f9b8df8e0b421a66330b5663f9780a66a243
David Dvořák <dejviddvorak146@gmail.com>
2021-10-30 20:32:44 +0200

    update appbar

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,46 @@
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import FilterDramaIcon from '@mui/icons-material/FilterDrama';
+import SearchBar from './SearchBar';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export default () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar color="secondary" position="static">
+                <Toolbar>
+
+                    <IconButton
+                        sx={{ display: { xs: 'block', sm: 'none' } }}
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                
+                    <Typography variant="h6" component="div" style={{margin: "10px"}} sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        yourCloud
+                    </Typography>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <FilterDramaIcon sx={{ fontSize: "30px", display: { xs: 'none', sm: 'block' } }} />
+                    </Box>
+                    <SearchBar />
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <AccountCircleIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,49 +1,11 @@
-import React, { useState } from 'react';
-import Toolbar from '@mui/material/Toolbar';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import FilterDramaIcon from '@mui/icons-material/FilterDrama';
-import Button from '@mui/material/Button';
-import SearchBar from './SearchBar';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AppBar from "./AppBar";
 
 
 export default () => {
 
     return (
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar color="secondary" position="static">
-                    <Toolbar>
-
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                    
-                        <Typography variant="h6" component="div" style={{margin: "10px"}}>
-                            yourCloud
-                        </Typography>
-                        <FilterDramaIcon sx={{ fontSize: "30px", flexGrow: 1}} />
-                        <SearchBar />
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                        >
-                            <AccountCircleIcon />
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-            </Box>
+            <AppBar />
         </>
     );
 }
