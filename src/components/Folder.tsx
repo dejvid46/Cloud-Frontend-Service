df0a6f103ada05eb595f90db4e138c6f07afd826
David Dvořák <dejviddvorak146@gmail.com>
2022-01-01 17:32:37 +0100

    add upload button if table is empty

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,6 +1,10 @@
 import FolderTable from './FolderTable';
+import { tableData } from './FolderTable';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 // name date size
-const rows = [
+const rows: tableData[] = [
     {id: 0, name: "cat.jpg", modified: "11. 2. 2018", type: "folder", size: 0 },
     {id: 1, name: "text.txt", modified: "11. 2. 2018", type: "folder", size: 0 },
     {id: 2, name: "pipipupi.mp3", modified: "11. 2. 2018", type: "folder", size: 0 },
@@ -9,7 +13,6 @@ const rows = [
     {id: 5, name: "Kuku2", modified: "11. 2. 2018", type: "folder", size: 0 },
     {id: 6, name: "Kuku2", modified: "11. 2. 2018", type: "folder", size: 0 },
     {id: 7, name: "Kuku2", modified: "11. 2. 2018", type: "folder", size: 0 },
-
 ];
 
 export default () => {
@@ -22,7 +25,27 @@ export default () => {
         <>
             <div id="tableDiv" style={{ height: `${tableHeight}px`, width: '100%' }}>
                 
-                <FolderTable table={rows} rowsCount={rowsCount} />
+                {rows.length !== 0 ? 
+                    <FolderTable table={rows} rowsCount={rowsCount} /> 
+                    : 
+                    <>
+                        <Grid
+                            container
+                            spacing={0}
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="center"
+                            style={{ minHeight: '70vh' }}
+                        >
+                            <Grid item xs={3}>
+                                <Typography variant="h6" component="div" style={{marginBottom: "5vh"}} >
+                                    Folder is empty
+                                </Typography>
+                                <Button variant="contained">Upload files</Button>
+                            </Grid>
+                        </Grid>
+                    </>
+                }
             </div>
         </>
     );
